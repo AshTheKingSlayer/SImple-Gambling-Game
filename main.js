bf6e647cc382a1b82552d416a47e47ccaa9a395e
ASH <adeeshapererabiz@gmail.com>
2024-10-20 04:48:29 +0530

    finish

--- main.js
+++ main.js
@@ -5,15 +5,64 @@ const LOWER=document.getElementById("LOWER");
 const DICE=document.getElementById("DICE");
 const MIN=1;
 const MAX=6;
+const answer=document.getElementById("ans");
+const display=document.getElementById("display");
+
 let result1;
 let result2;
+let choice;
+var sum;
 
-DICE.onclick = function(){
-    
-    result1=Math.floor(Math.random()*MAX)+MIN;
-    console.log(result1);
-    
-    result2=Math.floor(Math.random()*MAX)+MIN;
-    console.log(result2);
 
-}
+
+HIGHER.onclick = function() {
+    choice=1;
+
+    DICE.onclick = function(){
+    
+        result1=Math.floor(Math.random()*MAX)+MIN;
+        
+        
+        result2=Math.floor(Math.random()*MAX)+MIN;
+        
+        sum=result1+result2;
+        console.log(sum);
+        answer.textContent = `DICE SUM IS ${sum}`;
+
+
+        if(sum>8 && choice==1){
+            display.textContent="you won";
+            
+            }else{
+               display.textContent="you lost";
+            }
+    };
+
+};
+LOWER.onclick = function() {
+    choice=1; 
+
+    DICE.onclick = function(){
+    
+        result1=Math.floor(Math.random()*MAX)+MIN;
+        
+        
+        result2=Math.floor(Math.random()*MAX)+MIN;
+        
+        sum=result1+result2;
+        console.log(sum);
+        answer.textContent = `DICE SUM IS ${sum}`;
+
+
+        if(sum<8 && choice==1){
+            display.textContent="you won";
+            
+            }else{
+               display.textContent="you lost";
+            }
+    };
+};
+
+
+
+
